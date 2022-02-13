e32e3efe01bda96c7867c841dae3e2deaeaa29eb
screwball87 <screwballriver1987@gmail.com>
2022-02-13 15:03:13 +0000

    added first function in chapter 2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -8,4 +8,53 @@ if (hasDriversLicence) console.log("I can drive :-)");
 
 const interface = "Audio";
 const private = 534;
+
+const day = "tuesday";
+
+switch (day) {
+  case "monday":
+    console.log("it's monday");
+    break;
+  default:
+    console.log("it's not Monday");
+}
+
+const yourName = prompt("What is my name?");
+function logger() {
+  console.log(`My name is ${yourName}`);
+}
+
+// calling, running, or evoking a function
+
+logger();
+logger();
+logger();
+
+
+function foodProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples, and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = foodProcessor(5, 0);
+console.log(appleJuice);
+
+console.log(foodProcessor(5, 0));
+
+const appleOrangeJuice = foodProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+
+function fruitSelection(bananas, apples) {
+  const selection = Number(
+    prompt(
+      `How many bananas? ${bananas}` +
+        Number(prompt)`How many apples? ${apples}`
+    )
+  );
+  console.log(`I am using `);
+  return juice;
+}
+
+fruitSelection;
 */
